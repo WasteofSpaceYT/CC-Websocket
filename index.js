9d717fdeca6ea3d9c28acb697313ed89d2ec6a09
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-10-31 06:32:51 -0400

    making websocket

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws = __importStar(require("ws"));
+const server = new ws.Server({
+    port: 3000,
+});
+server.on('connection', (socket) => {
+    console.log('New connection');
+    console.log('Total connections', server.clients.size);
+    setTimeout(() => {
+        server.clients.forEach((client) => {
+            client.send('Hello from server');
+        });
+    }, 1000);
+    server.on("message", (message) => {
+        console.log(message);
+    });
+});
