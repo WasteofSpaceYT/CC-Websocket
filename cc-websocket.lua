32de7d2a0d331e3af01f0a7e6b7a65c0b7957b0a
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-11-10 15:55:16 -0500

    Update cc-websocket.lua

--- cc-websocket.lua
+++ cc-websocket.lua
@@ -1,20 +1,30 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local arg1, arg2 = ...
-if arg1 == "-p" or arg1 == "-port" then
-    if tonumber(arg2) then
-        port = arg2
-    else
-        port = "3000"
-    end
+local args = {...}
+if args.length == 2 and (args[1] == "-p" or args[1] == "-port") then
+ if tonumber(args[2]) then
+     port = args[2]
+ else
+     port = "3000"
+ end
+else
+ port = "3000"
 end
-ws = http.websocket("ws://localhost:" + port)
+local uri = "ws://localhost:3000"
+print(uri)
+ws = http.websocket(uri)
+if(type(ws) == "boolean") then
+    error("Failed to connect to websocket")
+end
+print(type(ws))
 while true do
         local event = os.pullEventRaw("terminate")
-        if event == "terminate" then 
+        if event == "terminate" and type(ws) == "table" then 
             ws.close()
-            error()
+            error("Terminated...")
         end
+        print(os.pullEventRaw())
+        if(type(ws) == "table") then
     local msg = ws.receive()
+    print(msg)
     local msgObj = {}
     local urlstartIndex, urlendIndex = string.find(msg, '"url":"')
     local url = string.sub(msg, urlendIndex + 1, string.find(msg, '"', urlendIndex + 1) - 1)
@@ -22,6 +32,7 @@ while true do
     local data = string.sub(msg, dataendIndex + 1, string.find(msg, '"', dataendIndex + 1) - 1)
     local typestartIndex, typeendIndex = string.find(msg, '"type":"')
     local type = string.sub(msg, typeendIndex + 1, string.find(msg, '"', typeendIndex + 1) - 1)
+    print(url.." "..data.." "..type)
 
         if type == "delete" then
             shell.run("delete " .. tostring(url))
@@ -38,4 +49,7 @@ while true do
             fs.open( url, "w").write( data)
             print("Updated "..url)
         end
+    else
+        print("Websocket is not started.")
+    end
 end
